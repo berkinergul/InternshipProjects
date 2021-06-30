9af7fe5b0cfabdbfd3e428531947e1f9ac005ac8
berkinergul <berkinergul99@gmail.com>
2021-06-30 21:03:41 +0300

    Project init

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mail,password;
+    Button buttonLogin;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mail = findViewById(R.id.edittxtMail);
+        password = findViewById(R.id.edittxtPassword);
+        buttonLogin = findViewById(R.id.btnLogin);
+        img = findViewById(R.id.imgMain);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userMail = mail.getText().toString();
+                String userPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(userMail)||TextUtils.isEmpty(userPassword)){
+                    Toast.makeText(MainActivity.this,"Please enter your informations!!",Toast.LENGTH_LONG).show();
+                }
+                else if(userMail.equals("example@gmail.com")&& userPassword.equals("1234")){
+                    Intent intent = new Intent(MainActivity.this,activity2.class);
+                    intent.putExtra("Mail",userMail);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(MainActivity.this,"Incorrect informations. Please try again!!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/project/activity2.java
@@ -0,0 +1,124 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class activity2 extends AppCompatActivity {
+
+    TextView welcoming,amountToPaid;
+    CheckBox tv,computer,tablet,phone;
+    RadioButton creditCard, payAtTheDoor,test;
+    Button buttonPurchase;
+    RadioGroup radiogroup;
+    int total = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        welcoming = findViewById(R.id.txtWelcoming);
+        amountToPaid = findViewById(R.id.txtAmount);
+        buttonPurchase = findViewById(R.id.btnPurchase);
+        tv = findViewById(R.id.checkTelevision);
+        computer = findViewById(R.id.checkComputer);
+        tablet = findViewById(R.id.checkTablet);
+        phone = findViewById(R.id.checkPhone);
+
+        radiogroup = findViewById(R.id.radioGroup);
+        creditCard = findViewById(R.id.radioCreditCard);
+        payAtTheDoor = findViewById(R.id.radioDoor);
+
+        Intent getInfo = getIntent();
+        String getMail = getInfo.getStringExtra("Mail");
+
+        welcoming.setText("Welcome, "+getMail);
+        Calculation();
+
+        buttonPurchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int selectedRadio = radiogroup.getCheckedRadioButtonId();
+               test = findViewById(selectedRadio);
+               Toast.makeText(activity2.this,"You are going to pay "+ test.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    public void Calculation(){
+        tv.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                total+=3000;
+                amountToPaid.setText(total + "$");
+            }
+            else
+            {
+                total-=3000;
+                amountToPaid.setText(total + "$");
+            }
+        });
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tv.isChecked()){
+                    total+=3000;
+                    amountToPaid.setText(total + "$");
+                }
+                else{
+                    total-=3000;
+                    amountToPaid.setText(total + "$");
+                }
+            }
+        });
+        computer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(computer.isChecked()){
+                    total+=4000;
+                    amountToPaid.setText(total + "$");
+                }
+                else{
+                    total-=4000;
+                    amountToPaid.setText(total + "$");
+                }
+            }
+        });
+        tablet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tablet.isChecked()){
+                    total+=1500;
+                    amountToPaid.setText(total + "$");
+                }
+                else{
+                    total-=1500;
+                    amountToPaid.setText(total + "$");
+                }
+            }
+        });
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(phone.isChecked()){
+                    total+=2000;
+                    amountToPaid.setText(total + "$");
+                }
+                else{
+                    total-=2000;
+                    amountToPaid.setText(total + "$");
+                }
+            }
+        });
+    }
+
+
+
+}
